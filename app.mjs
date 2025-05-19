6b58c5977820dcda3482a21efc842e2faa0bbf2c
whitepanguin <dun04045@gmail.com>
2025-05-19 14:51:26 +0900

    upload name

--- app.mjs
+++ app.mjs
@@ -43,7 +43,7 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
+    cb(null, file.originalname);
   },
 });
 
